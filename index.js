f71296d4d05f28d69a71ffd98b6472b9c1314213
Thomas Pope <tpope0928@gmail.com>
2020-03-15 23:19:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(namesArray, surprise){
+  let thankYouCards = []
+  for (let i = 0; i < namesArray.length; i++){
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${surprise} gift!`)
+  }
+  return thankYouCards
+}
+
+function countDown(countDown) {
+  while (countDown > 0) {
+    console.log(countDown);
+    countDown -=1;
+  }
+  console.log(countDown);
+}
